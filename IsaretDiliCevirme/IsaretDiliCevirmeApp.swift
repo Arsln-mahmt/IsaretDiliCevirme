e0d1bd7fe366398e258f5785d22369a68d3c16b8
Arsln-mahmt <arslnmahmt198@gmail.com>
2026-03-09 12:35:40 +0300

    Initial Commit

---
+++ IsaretDiliCevirme/IsaretDiliCevirmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsaretDiliCevirmeApp.swift
+//  IsaretDiliCevirme
+//
+//  Created by Mahmut Arslan on 9.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct IsaretDiliCevirmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
